47a137cd01d26346c70ce16b9bcbc323b437001f
Roman M <roma1109200@gmail.com>
2021-05-22 20:02:42 +0700

    Create .NET 5.0 project

---
+++ OptimalTrafic/OptimalTrafic/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace OptimalTrafic
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBoxOperator = new System.Windows.Forms.ComboBox();
+            this.textBoxSMS = new System.Windows.Forms.TextBox();
+            this.textBoxMinute = new System.Windows.Forms.TextBox();
+            this.Operator = new System.Windows.Forms.Label();
+            this.labelSMS = new System.Windows.Forms.Label();
+            this.labelMinute = new System.Windows.Forms.Label();
+            this.labelGB = new System.Windows.Forms.Label();
+            this.textBoxGB = new System.Windows.Forms.TextBox();
+            this.buttonSendSearchTarif = new System.Windows.Forms.Button();
+            this.erpTextBoxGB = new System.Windows.Forms.ErrorProvider(this.components);
+            this.erpTextBoxMinute = new System.Windows.Forms.ErrorProvider(this.components);
+            this.erpTextBoxSMS = new System.Windows.Forms.ErrorProvider(this.components);
+            this.erpButtonSendSearchTarif = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.erpTextBoxGB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erpTextBoxMinute)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erpTextBoxSMS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erpButtonSendSearchTarif)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxOperator
+            // 
+            this.comboBoxOperator.AutoCompleteCustomSource.AddRange(new string[] {
+            "МТС",
+            "Мегафон",
+            "Билайн"});
+            this.comboBoxOperator.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxOperator.FormattingEnabled = true;
+            this.comboBoxOperator.Items.AddRange(new object[] {
+            "МТС",
+            "Мегафон",
+            "Билайн"});
+            this.comboBoxOperator.Location = new System.Drawing.Point(402, 281);
+            this.comboBoxOperator.Name = "comboBoxOperator";
+            this.comboBoxOperator.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxOperator.TabIndex = 15;
+            // 
+            // textBoxSMS
+            // 
+            this.textBoxSMS.Location = new System.Drawing.Point(402, 244);
+            this.textBoxSMS.Name = "textBoxSMS";
+            this.textBoxSMS.Size = new System.Drawing.Size(100, 23);
+            this.textBoxSMS.TabIndex = 14;
+            this.textBoxSMS.Click += new System.EventHandler(this.textBoxSMS_Click);
+            this.textBoxSMS.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxSMS_KeyPress);
+            this.textBoxSMS.Leave += new System.EventHandler(this.textBoxSMS_Leave);
+            // 
+            // textBoxMinute
+            // 
+            this.textBoxMinute.Location = new System.Drawing.Point(402, 186);
+            this.textBoxMinute.Name = "textBoxMinute";
+            this.textBoxMinute.Size = new System.Drawing.Size(100, 23);
+            this.textBoxMinute.TabIndex = 13;
+            this.textBoxMinute.Click += new System.EventHandler(this.textBoxMinute_Click);
+            this.textBoxMinute.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxMinute_KeyPress);
+            this.textBoxMinute.Leave += new System.EventHandler(this.textBoxMinute_Leave);
+            // 
+            // Operator
+            // 
+            this.Operator.AutoSize = true;
+            this.Operator.Location = new System.Drawing.Point(277, 281);
+            this.Operator.Name = "Operator";
+            this.Operator.Size = new System.Drawing.Size(61, 15);
+            this.Operator.TabIndex = 11;
+            this.Operator.Text = "Оператор";
+            // 
+            // labelSMS
+            // 
+            this.labelSMS.AutoSize = true;
+            this.labelSMS.Location = new System.Drawing.Point(277, 244);
+            this.labelSMS.Name = "labelSMS";
+            this.labelSMS.Size = new System.Drawing.Size(102, 15);
+            this.labelSMS.TabIndex = 10;
+            this.labelSMS.Text = "Количество СМС";
+            // 
+            // labelMinute
+            // 
+            this.labelMinute.AutoSize = true;
+            this.labelMinute.Location = new System.Drawing.Point(277, 186);
+            this.labelMinute.Name = "labelMinute";
+            this.labelMinute.Size = new System.Drawing.Size(109, 15);
+            this.labelMinute.TabIndex = 9;
+            this.labelMinute.Text = "Количество минут";
+            // 
+            // labelGB
+            // 
+            this.labelGB.AutoSize = true;
+            this.labelGB.Location = new System.Drawing.Point(277, 128);
+            this.labelGB.Name = "labelGB";
+            this.labelGB.Size = new System.Drawing.Size(88, 15);
+            this.labelGB.TabIndex = 8;
+            this.labelGB.Text = "Количество ГБ";
+            // 
+            // textBoxGB
+            // 
+            this.textBoxGB.Location = new System.Drawing.Point(402, 125);
+            this.textBoxGB.Name = "textBoxGB";
+            this.textBoxGB.Size = new System.Drawing.Size(100, 23);
+            this.textBoxGB.TabIndex = 16;
+            this.textBoxGB.Click += new System.EventHandler(this.textBoxGB_Click);
+            this.textBoxGB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxGB_KeyPress);
+            this.textBoxGB.Leave += new System.EventHandler(this.textBoxGB_Leave);
+            // 
+            // buttonSendSearchTarif
+            // 
+            this.buttonSendSearchTarif.Location = new System.Drawing.Point(402, 329);
+            this.buttonSendSearchTarif.Name = "buttonSendSearchTarif";
+            this.buttonSendSearchTarif.Size = new System.Drawing.Size(75, 23);
+            this.buttonSendSearchTarif.TabIndex = 17;
+            this.buttonSendSearchTarif.Text = "Искать";
+            this.buttonSendSearchTarif.UseVisualStyleBackColor = true;
+            this.buttonSendSearchTarif.Click += new System.EventHandler(this.buttonSendSearchTarif_Click);
+            // 
+            // erpTextBoxGB
+            // 
+            this.erpTextBoxGB.ContainerControl = this;
+            // 
+            // erpTextBoxMinute
+            // 
+            this.erpTextBoxMinute.ContainerControl = this;
+            // 
+            // erpTextBoxSMS
+            // 
+            this.erpTextBoxSMS.ContainerControl = this;
+            // 
+            // erpButtonSendSearchTarif
+            // 
+            this.erpButtonSendSearchTarif.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSendSearchTarif);
+            this.Controls.Add(this.textBoxGB);
+            this.Controls.Add(this.comboBoxOperator);
+            this.Controls.Add(this.textBoxSMS);
+            this.Controls.Add(this.textBoxMinute);
+            this.Controls.Add(this.Operator);
+            this.Controls.Add(this.labelSMS);
+            this.Controls.Add(this.labelMinute);
+            this.Controls.Add(this.labelGB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.erpTextBoxGB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erpTextBoxMinute)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erpTextBoxSMS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.erpButtonSendSearchTarif)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxOperator;
+        private System.Windows.Forms.TextBox textBoxSMS;
+        private System.Windows.Forms.TextBox textBoxMinute;
+        private System.Windows.Forms.Label Operator;
+        private System.Windows.Forms.Label labelSMS;
+        private System.Windows.Forms.Label labelMinute;
+        private System.Windows.Forms.Label labelGB;
+        private System.Windows.Forms.TextBox textBoxGB;
+        private System.Windows.Forms.Button buttonSendSearchTarif;
+        private System.Windows.Forms.ErrorProvider erpTextBoxGB;
+        private System.Windows.Forms.ErrorProvider erpTextBoxMinute;
+        private System.Windows.Forms.ErrorProvider erpTextBoxSMS;
+        private System.Windows.Forms.ErrorProvider erpButtonSendSearchTarif;
+    }
+}
+
---
+++ OptimalTrafic/OptimalTrafic/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace OptimalTrafic
+{
+    public partial class Form1 : Form
+    {
+        public struct tarif
+        {
+            public string name;
+            public int minutes;
+            public int sms;
+            public int gigabytes;
+
+            public tarif(string _name, int _minutes, int _sms, int _gigabytes) : this()
+            {
+                this.name = _name;
+                this.minutes = _minutes;
+                this.sms = _sms;
+                this.gigabytes = _gigabytes;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxOperator.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int RequiredIndicators;
+        int StrokNumber;
+        int StrokSum;
+        int Difference;
+
+
+        private void buttonSendSearchTarif_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            if (string.IsNullOrEmpty(textBoxGB.Text) || string.IsNullOrWhiteSpace(textBoxGB.Text) ||
+                string.IsNullOrEmpty(textBoxSMS.Text) || string.IsNullOrWhiteSpace(textBoxSMS.Text) ||
+                string.IsNullOrEmpty(textBoxMinute.Text) || string.IsNullOrWhiteSpace(textBoxMinute.Text))
+            {
+                erpButtonSendSearchTarif.SetError(buttonSendSearchTarif, "Заполните поля");
+            }
+            else
+            {
+                erpButtonSendSearchTarif.Clear();
+                String tarifOperator = Convert.ToString(comboBoxOperator.Text);
+                String tarifOperatorFile = "";
+                switch (tarifOperator)
+                {
+                    case "МТС":
+                        tarifOperatorFile = "TarifsMTS.txt";
+                        break;
+                    case "Мегафон":
+                        tarifOperatorFile = "TarifsMegafon.txt";
+                        break;
+                    case "Билайн":
+                        tarifOperatorFile = "TarifsBilain.txt";
+                        break;
+                }
+
+                RequiredIndicators = Convert.ToInt32(textBoxGB.Text) + Convert.ToInt32(textBoxMinute.Text) + Convert.ToInt32(textBoxSMS.Text);
+
+                List<tarif> tarifs = new List<tarif>();
+                string[] s = File.ReadAllLines(tarifOperatorFile);
+                for (int i = 0; i < s.Length; i++)
+                {
+                    string[] rbtarif = s[i].Split(new char[] { ' ' });
+                    tarifs.Add(new tarif(Convert.ToString(rbtarif[0]), Convert.ToInt32(rbtarif[1]), Convert.ToInt32(rbtarif[2]), Convert.ToInt32(rbtarif[3])));
+                    StrokSum = Convert.ToInt32(rbtarif[1]) + Convert.ToInt32(rbtarif[2]) + Convert.ToInt32(rbtarif[3]);
+                    if (i == 0) Difference = Math.Abs(RequiredIndicators - StrokSum);
+                    if (Difference > Math.Abs(RequiredIndicators - StrokSum))
+                    {
+                        Difference = Math.Abs(RequiredIndicators - StrokSum);
+                        StrokNumber = i;
+                    }
+                }
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    textBoxGB.Text = Convert.ToString(tarifs[StrokNumber].name);
+                }
+
+
+                form2.label1.Text = Convert.ToString(tarifs[StrokNumber].name);
+                form2.label2.Text = Convert.ToString(tarifs[StrokNumber].minutes);
+                form2.label3.Text = Convert.ToString(tarifs[StrokNumber].sms);
+                form2.label4.Text = Convert.ToString(tarifs[StrokNumber].gigabytes);
+
+                List<String> slova = new List<string>();
+                this.Hide();
+                form2.Show();
+            }
+        }
+
+        private void textBoxGB_Leave(object sender, EventArgs e)
+        {
+            string pattern = "^([0-9])";
+            if (Regex.IsMatch(textBoxGB.Text, pattern))
+            {
+                erpTextBoxGB.Clear();
+                buttonSendSearchTarif.Enabled = true;
+            }
+            else
+            {
+                erpTextBoxGB.SetError(textBoxGB, "Введите количество гигабайт");
+                buttonSendSearchTarif.Enabled = false;
+                return;
+            }
+
+        }
+
+        private void textBoxMinute_Leave(object sender, EventArgs e)
+        {
+            string pattern = "^([0-9])";
+            if (Regex.IsMatch(textBoxMinute.Text, pattern))
+            {
+                erpTextBoxMinute.Clear();
+                buttonSendSearchTarif.Enabled = true;
+            }
+            else
+            {
+                erpTextBoxMinute.SetError(textBoxMinute, "Введите количество минут");
+                buttonSendSearchTarif.Enabled = false;
+                return;
+            }
+        }
+
+        private void textBoxSMS_Leave(object sender, EventArgs e)
+        {
+
+            string pattern = "^([0-9])";
+            if (Regex.IsMatch(textBoxSMS.Text, pattern))
+            {
+                erpTextBoxSMS.Clear();
+                buttonSendSearchTarif.Enabled = true;
+            }
+            else
+            {
+                erpTextBoxSMS.SetError(textBoxSMS, "Введите количество смс");
+                buttonSendSearchTarif.Enabled = false;
+                return;
+            }
+        }
+
+        private void textBoxGB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsNumber(e.KeyChar))
+            {
+                e.KeyChar = (char)0;
+                return;
+            }
+        }
+
+        private void textBoxMinute_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsNumber(e.KeyChar))
+            {
+                e.KeyChar = (char)0;
+                return;
+            }
+        }
+
+        private void textBoxSMS_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsNumber(e.KeyChar))
+            {
+                e.KeyChar = (char)0;
+                return;
+            }
+        }
+
+        private void textBoxGB_Click(object sender, EventArgs e)
+        {
+            textBoxGB.Clear();
+        }
+
+        private void textBoxMinute_Click(object sender, EventArgs e)
+        {
+            textBoxMinute.Clear();
+        }
+
+        private void textBoxSMS_Click(object sender, EventArgs e)
+        {
+            textBoxSMS.Clear();
+        }
+    }
+}
